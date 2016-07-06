4e3d0c658f68a887c0bd18909c9197c2136c7af7
keik <k4t0.kei@gmail.com>
2016-07-07 00:20:52 +0900

    Add index and some validations for User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630131330) do
+ActiveRecord::Schema.define(version: 20160707044920) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20160630131330) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
