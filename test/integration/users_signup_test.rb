996b00b1682495b8aee4185b52ce72f7a187084a
keik <k4t0.kei@gmail.com>
2016-07-08 00:42:21 +0900

    Refactor

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
              password_confirmation: "bar"
            }
     end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors', count: 8
   end
 
   test "valid signup information" do
@@ -25,6 +28,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                         }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 
 end
