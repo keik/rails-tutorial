110f1101a5f2ca5153391e8a246020b16259bf3d
keik <k4t0.kei@gmail.com>
2016-07-13 23:46:33 +0900

    [WIP] Implementing authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 # controller for users pages
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -36,5 +38,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    return if logged_in?
+    flash[:danger] = 'Please log in'
+    redirect_to login_url
   end
 end
