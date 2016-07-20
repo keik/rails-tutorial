257c45c98cca6faeffecada7aa036804b36da667
keik <k4t0.kei@gmail.com>
2016-07-21 00:49:09 +0900

    Add tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -56,4 +56,15 @@ class UsersControllerTest < ActionController::TestCase
     end
     assert_redirected_to root_url
   end
+
+  test 'should not allow admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { name: 'eee',
+                                            password: '',
+                                            password_confirmation: '',
+                                            admin: true }
+    @other_user.reload
+    assert_not @other_user.admin?
+  end
 end
