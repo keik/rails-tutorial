b25a8769e3f4d9925520ae82105ee8a54836fc57
keik <k4t0.kei@gmail.com>
2016-07-22 08:35:52 +0900

    Add activation field to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # model of user
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   before_save do
     self.email = email.downcase
@@ -45,5 +47,16 @@ class User < ActiveRecord::Base
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
