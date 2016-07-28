83bfc52fd2f7e854157d1b656effd75072a0099a
keik <k4t0.kei@gmail.com>
2016-07-29 00:40:30 +0900

    Implement password reset action and mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # model of user
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -66,12 +66,17 @@ class User < ActiveRecord::Base
     self.activation_digest = User.digest(activation_token)
   end
 
-  def activated
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
-
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, User.digest(reset_token))
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 end
