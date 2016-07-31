d053331aeb5abf4f201eff1a2b529749ded0b31f
keik <k4t0.kei@gmail.com>
2016-07-31 22:29:51 +0900

    Add tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,8 +55,18 @@ class User < ActiveRecord::Base
     update_attribute(:activated_at, Time.zone.now)
   end
 
-  def password_reset_expired
-    reset_sent_at < 2.hours.ago
+  def password_reset_expired?
+    # reset_sent_at < 2.hours.ago
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, User.digest(reset_token))
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
@@ -72,15 +82,5 @@ class User < ActiveRecord::Base
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
-  end
-
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, User.digest(reset_token))
-  end
-
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
   end
 end
