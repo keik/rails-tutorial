a9bb25d4ad40ad79c2b7ab3d58d8961867400c10
keik <k4t0.kei@gmail.com>
2016-08-01 23:39:04 +0900

    Fix to not show unactivated users

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,6 @@ User.create!(name:  'Example User',
                email: email,
                password:              password,
                password_confirmation: password,
-               activated: true,
+               activated: n.even?,
                activated_at: Time.zone.now)
 end
